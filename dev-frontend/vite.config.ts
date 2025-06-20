db95ac62fd5b339f75ba913220f971692efab330
vaibhavCodian <vaibhavshukla.try@gmail.com>
2025-06-20 22:12:53 +0530

    fix

--- dev-frontend/vite.config.ts
+++ dev-frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dev-frontend/', // Set this to your repo name for GitHub Pages
+  base: '/fincortex-zombie-analyzer/dev-frontend/', // Set this to your repo name for GitHub Pages
   build: {
     outDir: 'dist'
   }
